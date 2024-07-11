f3c93e4214476de004061ce6394286d5d0e905b5
Ilham Hafidz <xxspanzie@gmail.com>
2024-07-11 17:27:22 +0700

    Add prefix file and pagination on API get module & article

--- app/Http/Controllers/Api/ArticleController.php
+++ app/Http/Controllers/Api/ArticleController.php
@@ -10,13 +10,23 @@ class ArticleController extends Controller
 {
     public function __invoke()
     {
-        $articles = Article::all();
+        $articles = Article::paginate(5);
         
         if($articles){
+
+            $modifiedData = $articles    ->map(function($item) {
+                $item->file = "https://genzedu.id/storage/" . $item->banner;
+                return $item;
+            });
+
             return response()->json([
                 "status" => "EDU-001",
                 "message" => "Artikel berhasil di Get",
-                "data" => $articles
+                "data" => $modifiedData,
+                'current_page' => $articles->currentPage(),
+                'last_page' => $articles->lastPage(),
+                'per_page' => $articles->perPage(),
+                'total' => $articles->total(),
             ]);
         }
         return response()->json([
--- app/Http/Controllers/Api/ModuleController.php
+++ app/Http/Controllers/Api/ModuleController.php
@@ -10,13 +10,23 @@ class ModuleController extends Controller
 {
     public function __invoke()
     {
-        $modules = Module::all();
+        $modules = Module::paginate(5);
         
         if($modules){
+
+            $modifiedData = $modules    ->map(function($item) {
+                $item->file = "https://genzedu.id/storage/" . $item->file;
+                return $item;
+            });
+
             return response()->json([
                 "status" => "EDU-001",
                 "message" => "Modul Ajar berhasil di Get",
-                "data" => $modules
+                "data" => $modifiedData,
+                'current_page' => $modules->currentPage(),
+                'last_page' => $modules->lastPage(),
+                'per_page' => $modules->perPage(),
+                'total' => $modules->total(),
             ]);
         }
         return response()->json([
