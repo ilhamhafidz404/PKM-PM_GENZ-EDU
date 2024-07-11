e37a0f3213c59104184feed74d5c73a890d2029a
Ilham Hafidz <xxspanzie@gmail.com>
2024-07-11 16:52:19 +0700

    Auth API login siswa

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -4,25 +4,50 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Parents;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
     public function login(Request $request){
-        $parent = Parents::where("email", "=", $request->email)->first();
+        if($request->type == 1){
+            $user = User::where("nisn", "=", $request->uname)->first();
         
-        if ($parent && Hash::check($request->password, $parent->password)) {
+            if ($user && Hash::check($request->password, $user->password)) {
+                return response()->json([
+                    "status" => "EDU-001",
+                    "message" => "Berhasil login sebagai Siswa",
+                    "data" => $user
+                ]);
+            }
+
             return response()->json([
-                "status" => "EDU-001",
-                "message" => "Berhasil login",
-                "data" => $parent
+                "status" => "EDU-002",
+                "message" => "Akun Siswa tidak ditemukan",
+                "data" => (object) []
+            ]);
+        } else if ($request->type == 2){
+            $parent = Parents::where("email", "=", $request->uname)->first();
+        
+            if ($parent && Hash::check($request->password, $parent->password)) {
+                return response()->json([
+                    "status" => "EDU-001",
+                    "message" => "Berhasil login",
+                    "data" => $parent
+                ]);
+            }
+
+            return response()->json([
+                "status" => "EDU-002",
+                "message" => "Akun tidak ditemukan",
+                "data" => (object) []
             ]);
         }
 
         return response()->json([
-            "status" => "EDU-002",
-            "message" => "Akun tidak ditemukan",
+            "status" => "EDU-003",
+            "message" => "Error, type tidak sesuai",
             "data" => (object) []
         ]);
     }
